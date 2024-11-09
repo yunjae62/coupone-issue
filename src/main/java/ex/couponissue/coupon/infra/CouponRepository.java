63908113bf4fc0eb3a9396ec97a3fd89342d331e
yunjae62 <wodbs7758@naver.com>
2024-11-09 21:41:38 +0900

    feat: 비관적락 추가

--- src/main/java/ex/couponissue/coupon/infra/CouponRepository.java
+++ src/main/java/ex/couponissue/coupon/infra/CouponRepository.java
@@ -1,8 +1,15 @@
 package ex.couponissue.coupon.infra;
 
 import ex.couponissue.coupon.domain.Coupon;
+import jakarta.persistence.LockModeType;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CouponRepository extends JpaRepository<Coupon, String> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c FROM Coupon c WHERE c.id = :id")
+    Optional<Coupon> findByIdWithPessimisticLock(String id);
 }
