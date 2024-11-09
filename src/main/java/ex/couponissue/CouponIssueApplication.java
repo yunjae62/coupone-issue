3210b8f8ce63c920c642add4750cd0e0006bbe33
yunjae62 <wodbs7758@naver.com>
2024-11-09 16:30:02 +0900

    first commit

---
+++ src/main/java/ex/couponissue/CouponIssueApplication.java
@@ -0,0 +1,12 @@
+package ex.couponissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponIssueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponIssueApplication.class, args);
+    }
+}
---
+++ src/test/java/ex/couponissue/CouponIssueApplicationTests.java
@@ -0,0 +1,12 @@
+package ex.couponissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponIssueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
