8a0e8455d6d4e5fa7edd835b5e151de42468ef92
yunjae62 <wodbs7758@naver.com>
2024-11-09 18:18:38 +0900

    feat: 쿠폰 발급 서비스 추가
    - 쿠폰 발급 로직 추가

---
+++ src/main/java/ex/couponissue/coupon/service/CouponIssueService.java
@@ -0,0 +1,36 @@
+package ex.couponissue.coupon.service;
+
+import ex.couponissue.coupon.domain.Coupon;
+import ex.couponissue.coupon.domain.CouponIssue;
+import ex.couponissue.coupon.infra.CouponIssueRepository;
+import ex.couponissue.coupon.infra.CouponRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
+
+@Slf4j(topic = "CouponIssueService")
+@Service
+@RequiredArgsConstructor
+public class CouponIssueService {
+
+    private final CouponRepository couponRepository;
+    private final CouponIssueRepository couponIssueRepository;
+
+    /**
+     * 쿠폰 발급
+     */
+    @Transactional
+    public void issue(String couponId, String userId) {
+        // 쿠폰 조회
+        Coupon coupon = couponRepository.findById(couponId)
+            .orElseThrow(() -> new HttpClientErrorException(HttpStatusCode.valueOf(404), "존재하지 않는 쿠폰입니다."));
+
+        // 쿠폰 발급
+        coupon.issue();
+        CouponIssue couponIssue = CouponIssue.create(userId, coupon);
+        couponIssueRepository.save(couponIssue);
+    }
+}
