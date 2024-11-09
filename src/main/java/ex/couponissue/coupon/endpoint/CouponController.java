a59b135f0b8df13bfb87d50c96e0399b8654e8de
yunjae62 <wodbs7758@naver.com>
2024-11-09 17:29:59 +0900

    feat: 쿠폰 컨트롤러 추가
    - 쿠폰 생성 API 추가

---
+++ src/main/java/ex/couponissue/coupon/endpoint/CouponController.java
@@ -0,0 +1,30 @@
+package ex.couponissue.coupon.endpoint;
+
+import ex.couponissue.coupon.dto.request.CouponCreateReq;
+import ex.couponissue.coupon.dto.response.CouponGetRes;
+import ex.couponissue.coupon.service.CouponService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j(topic = "CouponController")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/coupons")
+public class CouponController {
+
+    private final CouponService couponService;
+
+    /**
+     * 쿠폰 생성
+     */
+    @PostMapping
+    public ResponseEntity<CouponGetRes> createCoupon(@RequestBody CouponCreateReq request) {
+        CouponGetRes response = couponService.createCoupon(request);
+        return ResponseEntity.ok(response);
+    }
+}
