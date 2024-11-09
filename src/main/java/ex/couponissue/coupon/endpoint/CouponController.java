2736a2bb401ef9f4fdd93cfec935b9f5a4fe7bfe
yunjae62 <wodbs7758@naver.com>
2024-11-09 17:54:12 +0900

    feat: 쿠폰 컨트롤러에 쿠폰 단건 조회 로직 추가

--- src/main/java/ex/couponissue/coupon/endpoint/CouponController.java
+++ src/main/java/ex/couponissue/coupon/endpoint/CouponController.java
@@ -6,6 +6,8 @@ import ex.couponissue.coupon.service.CouponService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CouponController {
 
     private final CouponService couponService;
+
+    /**
+     * 쿠폰 단건 조회
+     */
+    @GetMapping("/{couponId}")
+    public ResponseEntity<CouponGetRes> getCoupon(@PathVariable String couponId) {
+        CouponGetRes response = couponService.getCoupon(couponId);
+        return ResponseEntity.ok(response);
+    }
 
     /**
      * 쿠폰 생성
