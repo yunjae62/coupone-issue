6902474f75506b4b3d10ab9724cb79bb985b173f
yunjae62 <wodbs7758@naver.com>
2024-11-10 00:28:45 +0900

    fix: 낙관적 락 예외일 경우에만 재시도

--- src/main/java/ex/couponissue/coupon/service/CouponIssueOptimisticLockFacade.java
+++ src/main/java/ex/couponissue/coupon/service/CouponIssueOptimisticLockFacade.java
@@ -1,7 +1,9 @@
 package ex.couponissue.coupon.service;
 
+import jakarta.persistence.OptimisticLockException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 
 @Slf4j(topic = "CouponIssueOptimisticLockFacade")
@@ -12,11 +14,18 @@ public class CouponIssueOptimisticLockFacade {
     private final CouponIssueService couponIssueService;
 
     public void issue(String couponId, String userId) {
+        int count = 0;
         while (true) {
+            count++;
+//            if (count > 10) {
+//                log.error("쿠폰 발급에 실패했습니다.");
+//                throw new RuntimeException("쿠폰 발급에 실패했습니다.");
+//            }
             try {
                 couponIssueService.issueWithOptimisticLock(couponId, userId);
+                log.info("쿠폰 발급 완료 (시도 횟수: {})", count);
                 break;
-            } catch (Exception e) {
+            } catch (ObjectOptimisticLockingFailureException | OptimisticLockException e) {
                 try {
                     Thread.sleep(50);
                 } catch (InterruptedException ignored) {
