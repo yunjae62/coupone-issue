fbb35aa6b042c414eaa7150241526cda9818f78b
yunjae62 <wodbs7758@naver.com>
2024-11-10 16:40:52 +0900

    config: LuaScript를 빈으로 등록

--- src/main/java/ex/couponissue/config/RedisConfig.java
+++ src/main/java/ex/couponissue/config/RedisConfig.java
@@ -6,10 +6,13 @@ import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.RedisScript;
 
 @Configuration
 public class RedisConfig {
@@ -45,5 +48,11 @@ public class RedisConfig {
     @Bean
     public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         return new StringRedisTemplate(redisConnectionFactory);
+    }
+
+    @Bean
+    public RedisScript<String> script() {
+        Resource script = new ClassPathResource("/coupon-issue.lua");
+        return RedisScript.of(script, String.class);
     }
 }
