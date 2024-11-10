36f004379015d3e0577a4c73d8195d7421206d16
yunjae62 <wodbs7758@naver.com>
2024-11-10 16:44:12 +0900

    feat: LuaScript + Kafka 이용하는 쿠폰 발급 로직 추가

--- src/main/java/ex/couponissue/coupon/service/CouponIssueService.java
+++ src/main/java/ex/couponissue/coupon/service/CouponIssueService.java
@@ -3,11 +3,13 @@ package ex.couponissue.coupon.service;
 import com.github.ksuid.KsuidGenerator;
 import ex.couponissue.coupon.domain.Coupon;
 import ex.couponissue.coupon.domain.CouponIssue;
+import ex.couponissue.coupon.dto.request.CouponIssueReq;
 import ex.couponissue.coupon.infra.CouponIssueRepository;
 import ex.couponissue.coupon.infra.CouponRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,8 +20,10 @@ import org.springframework.web.client.HttpClientErrorException;
 @RequiredArgsConstructor
 public class CouponIssueService {
 
+    private final RedisService redisService;
     private final CouponRepository couponRepository;
     private final CouponIssueRepository couponIssueRepository;
+    private final KafkaTemplate<String, Object> kafkaTemplate;
 
     /**
      * 쿠폰 발급 - 비관적 락
@@ -64,5 +68,18 @@ public class CouponIssueService {
         coupon.issue();
         CouponIssue couponIssue = CouponIssue.create(KsuidGenerator.generate(), userId, coupon);
         couponIssueRepository.save(couponIssue);
+    }
+
+    /**
+     * 쿠폰 발급 - LuaScript + Kafka
+     */
+    public void issueWithLuaScriptAndKafka(String couponId, String userId) {
+        // Redis+LuaScript를 이용한 쿠폰 발급 줄세우기
+        int maxQuantity = redisService.getMaxQuantity(couponId); // 캐싱된 쿠폰의 최대 발급량 조회
+        redisService.issueInRedis(couponId, userId, maxQuantity); // 레디스에 쿠폰 발급 요청 저장
+
+        // Kafka를 시용한 실제 쿠폰 발급하기
+        CouponIssueReq couponIssueReq = new CouponIssueReq(couponId, userId);
+        kafkaTemplate.send("coupon.issue", couponIssueReq);
     }
 }
