032f25dcb0f390ca0fe70fb33e3414f396da3cb2
yunjae62 <wodbs7758@naver.com>
2024-11-10 16:38:55 +0900

    feat: 카프카 프로듀서 설정 추가

---
+++ src/main/java/ex/couponissue/config/KafkaProducerConfig.java
@@ -0,0 +1,32 @@
+package ex.couponissue.config;
+
+import java.util.Map;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> configProperties = Map.of(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class,
+            JsonSerializer.ADD_TYPE_INFO_HEADERS, false
+        );
+
+        return new DefaultKafkaProducerFactory<>(configProperties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
