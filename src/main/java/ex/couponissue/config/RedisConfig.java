b8f78a430b752766bc0a1806b893c2fccfa31d3b
yunjae62 <wodbs7758@naver.com>
2024-11-10 16:41:39 +0900

    config: 레디스 캐싱 매니저 설정

--- src/main/java/ex/couponissue/config/RedisConfig.java
+++ src/main/java/ex/couponissue/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package ex.couponissue.config;
 
+import java.time.Duration;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
@@ -8,11 +9,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -54,5 +59,19 @@ public class RedisConfig {
     public RedisScript<String> script() {
         Resource script = new ClassPathResource("/coupon-issue.lua");
         return RedisScript.of(script, String.class);
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig() // 기본 설정
+            .serializeKeysWith(SerializationPair.fromSerializer(RedisSerializer.string())) // 키는 문자열
+            .serializeValuesWith(SerializationPair.fromSerializer(RedisSerializer.string())) // 직렬화
+            .entryTtl(Duration.ofHours(1L)) // 기본 캐싱 TTL 설정
+            .disableCachingNullValues(); // null 캐싱 불가
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+            .cacheDefaults(redisCacheConfiguration)
+            .build();
     }
 }
