4c7908212ffe5cdeb6c1768388a2b54b39e39684
yunjae62 <wodbs7758@naver.com>
2024-11-10 13:48:07 +0900

    feat: redisson 분산락 파사드 객체 추가

---
+++ src/main/java/ex/couponissue/coupon/service/CouponIssueDistributeLockFacade.java
@@ -0,0 +1,40 @@
+package ex.couponissue.coupon.service;
+
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Service;
+
+@Slf4j(topic = "CouponIssueDistributeLockFacade")
+@Service
+@RequiredArgsConstructor
+public class CouponIssueDistributeLockFacade {
+
+    private final RedissonClient redissonClient;
+    private final CouponIssueService couponIssueService;
+
+    public void issue(String couponId, String userId) {
+        String lockName = "lock_" + couponId;
+        RLock lock = redissonClient.getLock(lockName);
+
+        try {
+            boolean available = lock.tryLock(10_000, 10_000, TimeUnit.MILLISECONDS);
+
+            if (!available) {
+                throw new IllegalArgumentException("[" + lockName + "] lock 획득 실패");
+            }
+
+            couponIssueService.issueWithDistributedLock(couponId, userId);
+
+        } catch (InterruptedException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                lock.unlock();
+            }
+        }
+    }
+}
