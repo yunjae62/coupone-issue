57ff1a1a769a9b3375edc882bccc4c9fb21f499b
yunjae62 <wodbs7758@naver.com>
2024-11-12 14:15:16 +0900

    feat: 분산락 AOP 적용

--- src/main/java/ex/couponissue/coupon/service/distribute/CouponIssueDistributeService.java
+++ src/main/java/ex/couponissue/coupon/service/distribute/CouponIssueDistributeService.java
@@ -1,6 +1,7 @@
 package ex.couponissue.coupon.service.distribute;
 
 import com.github.ksuid.KsuidGenerator;
+import ex.couponissue.aop.DistributedLock;
 import ex.couponissue.coupon.domain.Coupon;
 import ex.couponissue.coupon.domain.CouponIssue;
 import ex.couponissue.coupon.infra.CouponRepository;
@@ -24,6 +25,21 @@ public class CouponIssueDistributeService {
      */
     @Transactional(propagation = Propagation.REQUIRES_NEW, timeout = 15)
     public void issueWithDistributedLock(String couponId, String userId) {
+        // 쿠폰 조회
+        Coupon coupon = couponRepository.findById(couponId)
+            .orElseThrow(() -> new HttpClientErrorException(HttpStatusCode.valueOf(404), "존재하지 않는 쿠폰입니다."));
+
+        // 쿠폰 발급
+        CouponIssue couponIssue = CouponIssue.create(KsuidGenerator.generate(), userId, coupon);
+        coupon.issue(couponIssue);
+        couponRepository.saveAndFlush(coupon); // CascadeType.MERGE 로 인해 couponIssue 도 같이 저장됨
+    }
+
+    /**
+     * 쿠폰 발급 - 분산 락
+     */
+    @DistributedLock(key = "#couponId", waitTime = 10L, leaseTime = 10L)
+    public void issue(String couponId, String userId) {
         // 쿠폰 조회
         Coupon coupon = couponRepository.findById(couponId)
             .orElseThrow(() -> new HttpClientErrorException(HttpStatusCode.valueOf(404), "존재하지 않는 쿠폰입니다."));
