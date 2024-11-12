f8d0c04a95226ef4bb6c92af6fb6c09e19efbe5d
yunjae62 <wodbs7758@naver.com>
2024-11-12 14:16:18 +0900

    remove: 기존 객체 제거

--- src/main/java/ex/couponissue/coupon/service/distribute/CouponIssueDistributeService.java
+++ src/main/java/ex/couponissue/coupon/service/distribute/CouponIssueDistributeService.java
@@ -9,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpClientErrorException;
 
 @Slf4j(topic = "CouponIssueService")
@@ -19,21 +17,6 @@ import org.springframework.web.client.HttpClientErrorException;
 public class CouponIssueDistributeService {
 
     private final CouponRepository couponRepository;
-
-    /**
-     * 쿠폰 발급 - 분산 락
-     */
-    @Transactional(propagation = Propagation.REQUIRES_NEW, timeout = 15)
-    public void issueWithDistributedLock(String couponId, String userId) {
-        // 쿠폰 조회
-        Coupon coupon = couponRepository.findById(couponId)
-            .orElseThrow(() -> new HttpClientErrorException(HttpStatusCode.valueOf(404), "존재하지 않는 쿠폰입니다."));
-
-        // 쿠폰 발급
-        CouponIssue couponIssue = CouponIssue.create(KsuidGenerator.generate(), userId, coupon);
-        coupon.issue(couponIssue);
-        couponRepository.saveAndFlush(coupon); // CascadeType.MERGE 로 인해 couponIssue 도 같이 저장됨
-    }
 
     /**
      * 쿠폰 발급 - 분산 락
