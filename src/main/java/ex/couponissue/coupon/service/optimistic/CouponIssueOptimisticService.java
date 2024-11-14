92794580bcacb5284997021f41d650f58a8add7a
yunjae62 <wodbs7758@naver.com>
2024-11-14 17:00:13 +0900

    refactor: 낙관락 서비스에 어노테이션 적용

--- src/main/java/ex/couponissue/coupon/service/optimistic/CouponIssueOptimisticService.java
+++ src/main/java/ex/couponissue/coupon/service/optimistic/CouponIssueOptimisticService.java
@@ -1,6 +1,7 @@
 package ex.couponissue.coupon.service.optimistic;
 
 import com.github.ksuid.KsuidGenerator;
+import ex.couponissue.aop.optimistic.OptimisticLock;
 import ex.couponissue.coupon.domain.Coupon;
 import ex.couponissue.coupon.domain.CouponIssue;
 import ex.couponissue.coupon.infra.CouponRepository;
@@ -8,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpClientErrorException;
 
 @Slf4j(topic = "CouponIssueService")
@@ -21,10 +21,11 @@ public class CouponIssueOptimisticService {
     /**
      * 쿠폰 발급 - 낙관적 락
      * <p>
-     * ****** Coupon 엔티티에 `@Version` 주석 해제 해야 함!!!!!!!!!1 ******
+     * ****** Coupon 엔티티에 `@Version` 주석 해제 해야 함!!!!!!!!! ******
      */
-    @Transactional
-    public void issueWithOptimisticLock(String couponId, String userId) {
+//    @Transactional
+    @OptimisticLock
+    public void issue(String couponId, String userId) {
         // 쿠폰 조회
         Coupon coupon = couponRepository.findByIdWithOptimisticLock(couponId)
             .orElseThrow(() -> new HttpClientErrorException(HttpStatusCode.valueOf(404), "존재하지 않는 쿠폰입니다."));
