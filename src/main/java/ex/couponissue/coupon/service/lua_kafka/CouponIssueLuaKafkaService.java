a7a8d1c039320c8caf003af82d9401534a3c26b1
yunjae62 <wodbs7758@naver.com>
2024-11-18 00:28:39 +0900

    feat: 쿠폰이슈 엔티티만 생성하도록 수정

--- src/main/java/ex/couponissue/coupon/service/lua_kafka/CouponIssueLuaKafkaService.java
+++ src/main/java/ex/couponissue/coupon/service/lua_kafka/CouponIssueLuaKafkaService.java
@@ -1,17 +1,15 @@
 package ex.couponissue.coupon.service.lua_kafka;
 
 import com.github.ksuid.KsuidGenerator;
-import ex.couponissue.coupon.domain.Coupon;
-import ex.couponissue.coupon.domain.CouponIssue;
+import ex.couponissue.coupon.domain.lua_kafka.CouponForLuaKafka;
+import ex.couponissue.coupon.domain.lua_kafka.CouponIssueForLuaKafka;
 import ex.couponissue.coupon.dto.request.CouponIssueReq;
-import ex.couponissue.coupon.infra.CouponRepository;
+import ex.couponissue.coupon.infra.CouponIssueForLuaKafkaRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.HttpClientErrorException;
 
 @Slf4j(topic = "CouponIssueService")
 @Service
@@ -19,16 +17,16 @@ import org.springframework.web.client.HttpClientErrorException;
 public class CouponIssueLuaKafkaService {
 
     private final RedisService redisService;
-    private final CouponRepository couponRepository;
     private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final CouponIssueForLuaKafkaRepository couponIssueForLuaKafkaRepository;
 
     /**
      * 쿠폰 발급 - LuaScript + Kafka
      */
     public void issue(String couponId, String userId) {
         // Redis+LuaScript를 이용한 쿠폰 발급 줄세우기
-        int maxQuantity = redisService.getMaxQuantity(couponId); // 캐싱된 쿠폰의 최대 발급량 조회
-        redisService.issueInRedis(couponId, userId, maxQuantity); // 레디스에 쿠폰 발급 요청 저장
+        CouponForLuaKafka coupon = redisService.getCoupon(couponId);// 캐싱된 쿠폰의 최대 발급량 조회
+        redisService.issueInRedis(couponId, userId, coupon.getMaxQuantity()); // 레디스에 쿠폰 발급 요청 저장
 
         // Kafka를 시용한 실제 쿠폰 발급하기
         CouponIssueReq couponIssueReq = new CouponIssueReq(couponId, userId);
@@ -36,17 +34,12 @@ public class CouponIssueLuaKafkaService {
     }
 
     /**
-     * 쿠폰 발급 - 비관적 락
+     * 쿠폰 발급
      */
     @Transactional
-    public void issueWithPessimisticLock(String couponId, String userId) {
-        // 쿠폰 조회
-        Coupon coupon = couponRepository.findByIdWithPessimisticLock(couponId)
-            .orElseThrow(() -> new HttpClientErrorException(HttpStatusCode.valueOf(404), "존재하지 않는 쿠폰입니다."));
-
-        // 쿠폰 발급
-        CouponIssue couponIssue = CouponIssue.create(KsuidGenerator.generate(), userId, coupon);
-        coupon.issue(couponIssue);
-        couponRepository.saveAndFlush(coupon); // CascadeType.MERGE 로 인해 couponIssue 도 같이 저장됨
+    public void issueWithNoLock(String couponId, String userId) {
+        CouponForLuaKafka coupon = redisService.getCoupon(couponId);// 캐싱된 쿠폰의 최대 발급량 조회
+        CouponIssueForLuaKafka couponIssue = CouponIssueForLuaKafka.create(KsuidGenerator.generate(), userId, coupon);
+        couponIssueForLuaKafkaRepository.saveAndFlush(couponIssue);
     }
 }
