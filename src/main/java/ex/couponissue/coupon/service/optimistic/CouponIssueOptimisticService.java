42f161eb83c3c60e8b8c457c1037ab4570d06628
yunjae62 <wodbs7758@naver.com>
2024-11-17 23:49:24 +0900

    feat: 낙관락 서비스에 낙관락 전용 요소들로 수정

--- src/main/java/ex/couponissue/coupon/service/optimistic/CouponIssueOptimisticService.java
+++ src/main/java/ex/couponissue/coupon/service/optimistic/CouponIssueOptimisticService.java
@@ -2,9 +2,9 @@ package ex.couponissue.coupon.service.optimistic;
 
 import com.github.ksuid.KsuidGenerator;
 import ex.couponissue.aop.optimistic.OptimisticLock;
-import ex.couponissue.coupon.domain.Coupon;
-import ex.couponissue.coupon.domain.CouponIssue;
-import ex.couponissue.coupon.infra.CouponRepository;
+import ex.couponissue.coupon.domain.optimistic.CouponForOptimistic;
+import ex.couponissue.coupon.domain.optimistic.CouponIssueForOptimistic;
+import ex.couponissue.coupon.infra.CouponForOptimisticRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
@@ -16,22 +16,19 @@ import org.springframework.web.client.HttpClientErrorException;
 @RequiredArgsConstructor
 public class CouponIssueOptimisticService {
 
-    private final CouponRepository couponRepository;
+    private final CouponForOptimisticRepository couponRepository;
 
     /**
      * 쿠폰 발급 - 낙관적 락
-     * <p>
-     * ****** Coupon 엔티티에 `@Version` 주석 해제 해야 함!!!!!!!!! ******
      */
-//    @Transactional
     @OptimisticLock
     public void issue(String couponId, String userId) {
         // 쿠폰 조회
-        Coupon coupon = couponRepository.findByIdWithOptimisticLock(couponId)
+        CouponForOptimistic coupon = couponRepository.findByIdWithOptimisticLock(couponId)
             .orElseThrow(() -> new HttpClientErrorException(HttpStatusCode.valueOf(404), "존재하지 않는 쿠폰입니다."));
 
         // 쿠폰 발급
-        CouponIssue couponIssue = CouponIssue.create(KsuidGenerator.generate(), userId, coupon);
+        CouponIssueForOptimistic couponIssue = CouponIssueForOptimistic.create(KsuidGenerator.generate(), userId, coupon);
         coupon.issue(couponIssue);
         couponRepository.saveAndFlush(coupon); // CascadeType.MERGE 로 인해 couponIssue 도 같이 저장됨
     }
